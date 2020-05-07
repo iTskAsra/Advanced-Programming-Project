82d1689cbdb1ab23571e8d9f568ba9df836f3aa5
Kasra <kasra138079@gmail.com>
2020-05-08 04:16:31 +0430

    offpanel ended!

--- src/main/java/view/OffPanel.java
+++ src/main/java/view/OffPanel.java
@@ -51,29 +51,39 @@ public class OffPanel extends Menu {
                 Product theProduct = gson.fromJson(productGson, model.Product.class);
                 //TODO check the entered command
                 String buffer = scanner.nextLine();
-                Digest digest = new Digest("Digest",this);
+                //Digest digest = new Digest("Digest",this);
+                //ProductPageController productPageController = new ProductPageController(theProduct);
+                ProductPage productPage = new ProductPage("Product's Page",this.parentMenu);
                 if (buffer.equals("add to cart")){
-
+                    productPage.submenus.get(1).run(1);
                 }
                 else if(buffer.equals("digest")){
-
+                    productPage.submenus.get(1).show();
                 }
                 else if (buffer.startsWith("select seller")){
-                    String[] tempArray = buffer.split("\\s");
-                    String sellerName = tempArray[2];
+                    //String[] tempArray = buffer.split("\\s");
+                    //String sellerName = tempArray[2];
+                    //digest.run(0);
+                    productPage.submenus.get(1).run(0);
                 }
                 else if(buffer.equals("Comments")){
-
+                    productPage.submenus.get(4).showAllComments();
                 }
                 else if(buffer.equals("attributes")){
-
+                    productPage.submenus.get(2).show();
+                    productPage.submenus.get(2).run();
                 }
                 else if(buffer.startsWith(("compare"))){
                     //TODO check if the statement after compare is a number(handle number format exception)
-                    String[] tempArray = buffer.split("\\s");
-                    String secondaryProductIdInString = tempArray[1];
-                    int secondaryProductId = Integer.parseInt(secondaryProductIdInString);
-
+                    //String[] tempArray = buffer.split("\\s");
+                    //String secondaryProductIdInString = tempArray[1];
+                    //int secondaryProductId = Integer.parseInt(secondaryProductIdInString);
+                    productPage.submenus.get(3).show();
+                    productPage.submenus.get(3).run();
+                }
+                else if (buffer.equals"add comment"){
+                    productPage.submenus.get(4).addComment().show();
+                    productPage.submenus.get(4).addComment().run();
                 }
                 getParentMenu().show();
                 getParentMenu().run();
